aaa99d6617e2b407c10e7e997c85a74491b7ae83
Brian Duong <brianminhduong@gmail.com>
2022-07-13 22:37:36 -0400

    Fix NewTask to utilize FireBase Database

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     setError(null);
     try {
       const response = await fetch(
-        "https://custom-react-hooks-e9a21-default-rtdb.firebaseio.com/tasks.json"
+        "https://react-http-6e23b-default-rtdb.firebaseio.com/tasks.json"
       );
 
       if (!response.ok) {
